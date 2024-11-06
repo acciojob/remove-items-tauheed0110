5cb8ab82751a72bdb8d5393fe17bffc801bbc0b1
tauheed0110 <tauheed0110@gmail.com>
2024-11-06 10:52:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+
+const dropdown = document.getElementById("colorSelect");
+
+function remove(){
+    // dropdown.innerHTML = "";
+    for(let i=0; i<dropdown.clientHeight; i++){
+        if(dropdown.value === dropdown[i].value){
+            dropdown[i].remove();
+            break;
+        }
+    }
+}
